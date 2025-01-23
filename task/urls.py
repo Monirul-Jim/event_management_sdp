d6c370437ea591796c0d9202a246780add420db9
Monirul Jim <monirulislamjim6640@gmail.com>
2025-01-23 14:37:41 +0600

    define model

---
+++ task/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from task.views import home
+urlpatterns = [
+    path('', home, name='home'),
+]
--- task_managemnt/settings.py
+++ task_managemnt/settings.py
@@ -14,9 +14,9 @@ import environ
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 environ.Env.read_env()
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
