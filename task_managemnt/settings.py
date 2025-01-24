d620c80d8ac735650682e41d9cebc6180b3392de
Monirul Jim <monirulislamjim6640@gmail.com>
2025-01-24 21:23:42 +0600

    remove debug toolbar

--- task_managemnt/settings.py
+++ task_managemnt/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "debug_toolbar",
     'task',
 ]
 
@@ -52,13 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
-INTERNAL_IPS = [
-    # ...
-    "127.0.0.1",
-    # ...
-]
+
 ROOT_URLCONF = 'task_managemnt.urls'
 
 TEMPLATES = [
