e6ef83e806d2a258789f1a8f28d963938b153d9c
Monirul Jim <monirulislamjim6640@gmail.com>
2025-03-08 21:57:04 +0600

    added new database

--- task_managemnt/settings.py
+++ task_managemnt/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,11 +84,17 @@ WSGI_APPLICATION = 'task_managemnt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        config('DATABASE_URL')
+
+    )
 }
 # DATABASES = {
 #     'default': {
